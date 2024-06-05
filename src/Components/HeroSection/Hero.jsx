150271eb19b31badfc63e1963e0738648206c1eb
shubhamessier <shubham.gaur7116@gmail.com>
2024-06-05 17:10:06 +0530

    blur effect added

--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import styles from "./HeroStyle.css";
+import React, { useEffect, useRef } from "react";
+import "./HeroStyle.css";
 
 const HeroSection = () => {
+  const heroContentRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const heroContent = heroContentRef.current;
+      const heroHeight = document.querySelector(".hero").offsetHeight;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition > heroHeight) {
+        heroContent.style.opacity = 1 - (scrollPosition - heroHeight) / 200;
+      } else {
+        heroContent.style.opacity = 1;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <section className="hero">
         <div className="overlay"></div>
-        <div className="hero-content">
+        <div className="hero-content" ref={heroContentRef}>
           <h1>We're Anti AI, An Innovation Powerhouse.</h1>
           <p>
             We're An Innovative Software Company. Protecting Humanity from the{" "}
