9836f951f28352a526ec9b88e76c729240cc6080
shubhamessier <shubham.gaur7116@gmail.com>
2024-06-06 17:09:39 +0530

    added the about section

--- src/Components/ImageElement/About.jsx
+++ src/Components/ImageElement/About.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import "./AboutStyle.css";
 
-export default function App() {
+const ImageTextComponent = () => {
   return (
-    <div className="responsive-container-block bigContainer">
-      {" "}
-      <div className="responsive-container-block Container bottomContainer">
-        {" "}
-        <div className="ultimateImg">
-          {" "}
-          <img
-            className="mainImg"
-            src="https://plus.unsplash.com/premium_photo-1664041273166-58f5fdf2e6d3?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Creative Business People Meeting"
-          />{" "}
-        </div>{" "}
-        <div className="allText bottomText">
-          {" "}
-          <p className="headingText">Anti.0</p>{" "}
-          <p className="description">
-            {" "}
-            Anti.0 Is our first indegniously developed solutions against AI,
-            with it's boundless capabilites and functionalities, it's our first
-            step in the direction to mitigate AI on a smaller scale.{" "}
+    <div className="flex justify-center items-center h-screen bg-transparent-600 p-20">
+      <div className="flex-1">
+        <img
+          src="https://images.unsplash.com/photo-1716436330152-a58390897652?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-full object-cover rounded-2xl mb-40"
+        />
+      </div>
+      <div className="flex-1">
+        <div className="bg-gray-800 p-20 rounded-2xl">
+          <h3 className="text-3xl font-bold mb-20 text-white">ANTI.ZERO</h3>
+          <p className="text-lg text-gray-400 mb-20">
+            Our first Indigenously developed solution against Artificial
+            Intelligence, capable of mitigating browser based threats from any
+            system.
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default ImageTextComponent;
